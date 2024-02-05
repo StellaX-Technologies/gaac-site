a683a336ea35dfd55809c78a16f34acb9358ea12
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-02-05 22:14:31 +0530

    Changes to footer, made Team carousel

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,11 @@ import mail from '../public/mail.svg';
 import linkedin from '../public/linkedin.svg'; 
 import Link from 'next/link';
 import Image from 'next/image';
+import Carousel_team from '@/components/carouselTeam';
 
 export default function Home() {
   return (
-    <body>
+    <body className='font:Roboto'>
 
       <div className="inline-flex justify-center text-center w-full">
         <div className='hero-img bg-local h-full w-full'>
@@ -21,62 +22,70 @@ export default function Home() {
       </div>
 
       <div className='flex'>
-        <p className='text-gray-50 text-[21px] font-bold text-left mt-10 lg:text-[30px]'>Events</p>
+        <p className='text-gray-50 text-[1.3125rem] font-bold text-left mt-10 lg:text-[1.875rem]'>Events</p>
         <Carousel/>
       </div>
 
+      <div className='flex flex-col'>
+        <h1 className='text-gray-50 font-bold lg:text-[1.875rem] md:text-[1.313rem] sm:text-[1.313rem]'>Executive Body</h1>
+        <p className='text-gray-50 lg:text-[1.125rem] md:text-[1.125rem] sm:text-[1.00rem] mt-8'>The Gitam Aero Astro Club was established in Nov 2020. We aim at upholding the cognitive aspect of education by ensuring academic excellence and intellectual growth of its students.</p>
+        <span className='mt-12 justify-center'>
+          <Carousel_team/>
+        </span>
+      </div>
+
       <div className='flex'>
-        <p className='text-gray-50 text-[21px] font-bold text-left mt-10 lg:text-[30px]'>Gallery</p>
+        <p className='text-gray-50 text-[1.3125rem] font-bold text-left mt-10 lg:text-[1.875rem]'>Gallery</p>
         <div className='flex flex-row w-full my-8 overflow:auto'>
           <Gallery/>
         </div>
       </div>
 
-      <footer className='hidden lg:flex flex-row justify-evenly max-w-full bg-black pt-[77px] pb-[77px]'>
+      <footer className='flex flex-row justify-evenly bg-black w-full lg:pt-15% lg:pb-15% md:pt-10% md:pb-5% sm:pt-10% sm:pb-5%'>
         <div className='text-slate-50'>
-          <h2 className='font-bold text-[25px] pb-5'>EVENTS</h2>
-          <ul className='font-extralight'>
-            <li className='pb-2'><Link href='#'>Rocketry 101</Link></li>
-            <li className='pb-2'><Link href='#'>Rocketry 2.0</Link></li>       
-            <li className='pb-2'><Link href='#'>Diana's Eve</Link></li>
-            <li className='pb-2'><Link href='#'>FrostHack 2k23</Link></li>
-            <li className='pb-2'><Link href='#'>Sangharsh</Link></li>
+          <h2 className='font-bold lg:text-[1.5625rem] pb-5 md:text-[1.2625rem] sm:text-[1.00rem]'>EVENTS</h2>
+          <ul className='lg:font-extralight lg:text-[1.4625rem] md:font-extralight md:text-[1.4625rem] md:text-sm'>
+            <li className='pb-4'><Link href='#'>Rocketry 101</Link></li>
+            <li className='pb-4'><Link href='#'>Rocketry 2.0</Link></li>       
+            <li className='pb-4'><Link href='#'>Diana's Eve</Link></li>
+            <li className='pb-4'><Link href='#'>FrostHack 2k23</Link></li>
+            <li className='pb-4'><Link href='#'>Sangharsh</Link></li>
           </ul>
         </div>
         <div className='text-slate-50'>
-          <h2 className='font-bold text-[25px] pb-5'>ABOUT US</h2>
-          <ul className='font-extralight'>
-            <li className='pb-2'><Link href='#'>Teams</Link></li>
-            <li className='pb-2'><Link href='#'>Executive Body</Link></li>
-            <li className='pb-2'><Link href='#'>Advisors</Link></li>
-            <li className='pb-2'><Link href='#'>Careers</Link></li>
+          <h2 className='font-bold lg:text-[1.5625rem] pb-5 md:text-[1.2625rem] sm:text-[1.00rem]'>ABOUT US</h2>
+          <ul className='lg:font-extralight lg:text-[1.4625rem] md:font-extralight md:text-[1.4625rem] md:text-sm'>
+            <li className='pb-4'><Link href='#'>Teams</Link></li>
+            <li className='pb-4'><Link href='#'>Executive Body</Link></li>
+            <li className='pb-4'><Link href='#'>Advisors</Link></li>
+            <li className='pb-4'><Link href='#'>Careers</Link></li>
           </ul>
         </div>
         <div className='text-slate-50'>
-          <h2 className='font-bold text-[25px] pb-5'>SOCIAL MEDIA</h2>
-          <ul className='font-extralight pb-4' style={{ display: 'flex', alignItems: 'center' }}>
-            <li className='pr-4'>
-              <Image src={insta} alt='insta_logo' width={40} height={40} />
+          <h2 className='font-bold lg:text-[1.5625rem] pb-5 md:text-[1.2625rem] sm:text-[1.00rem]'>SOCIAL MEDIA</h2>
+          <ul className='lg:font-extralight lg:text-[1.4625rem]  md:font-extralight md:text-[1.4625rem] md:text-sm pb-4' style={{ display: 'flex', alignItems: 'center' }}>
+            <li className='lg:pr-4 md:pr-3 sm:pr-2'>
+              <Image src={insta} alt='Instagram logo' width={40} height={40} />
             </li>
-            <li className='pb-2'>
+            <li className='pb-4'>
               Instagram<br />
               <Link href='https://www.instagram.com/'><span className='underline decoration-solid'>g.aeroastro</span></Link>
             </li>
           </ul>
-          <ul className='font-extralight pb-4' style={{ display: 'flex', alignItems: 'center' }}>
-            <li className='pr-4'>
-              <Image src={mail} alt='mail_logo' width={40} height={40} />
+          <ul className='lg:font-extralight lg:text-[1.4625rem]  md:font-extralight md:text-[1.4625rem] md:text-sm pb-4' style={{ display: 'flex', alignItems: 'center' }}>
+            <li className='lg:pr-4 md:pr-3 sm:pr-2'>
+              <Image src={mail} alt='Mail logo' width={40} height={40} />
             </li>
-            <li className='pb-2'>
+            <li className='pb-4'>
               Email<br />
               <Link href='https://mail.google.com/'><span className='underline decoration-solid'>aeroastro_vzg@gitam.in</span></Link>
             </li>
           </ul>
-          <ul className='font-extralight pb-4' style={{ display: 'flex', alignItems: 'center' }}>
-            <li className='pr-4'>
-              <Image src={linkedin} alt='linkedin_logo' width={40} height={40} />
+          <ul className='lg:font-extralight lg:text-[1.4625rem]  md:font-extralight md:text-[1.4625rem] md:text-sm pb-4' style={{ display: 'flex', alignItems: 'center' }}>
+            <li className='lg:pr-4 md:pr-3 sm:pr-2'>
+              <Image src={linkedin} alt='Linkedin logo' width={40} height={40} />
             </li>
-            <li className='pb-2'>
+            <li className='pb-4'>
               Linkedin<br />
               <Link href='https://www.linkedin.com/'><span className='underline decoration-solid'>GITAM Aero Astro Club</span></Link>
             </li>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from 'next/image';
 import logo from '../public/navbar.png';
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex flex-col items-center justify-center lg:bg-navbar lg:bg-opacity-11 mt-[33px] p-6 rounded-3xl max-w-[1309px] mx-auto w-full">
+    <nav className="flex flex-col items-center justify-center lg:bg-navbar lg:bg-opacity-11 mt-[2.0625rem] p-6 rounded-3xl max-w-[81.8125rem] mx-auto w-full">
       <div className="flex items-center justify-between w-full">
         <div className="lg:hidden">
           <button onClick={toggleNavbar} className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-800 hover:border-gray-800">
@@ -26,27 +26,27 @@ const Navbar = () => {
           <Image src={logo} width={100} height={20} alt='logo' quality={100} />
         </div>
         <div className={`lg:flex lg:items-center lg:justify-between w-full ${isOpen ? 'block' : 'hidden'}`}>
-        <a href="#responsive-header" className="block m-2 ml-[50px] lg:inline-block text-white font-bold hover:drop-shadow-xl">
-              Home
-            </a>
-            <a href="#responsive-header" className="block m-2 ml-[55px] lg:inline-block text-white font-bold hover:drop-shadow-xl">
-              Team
-            </a>
-            <a href="#responsive-header" className="block m-2 ml-[55px] lg:inline-block text-white font-bold hover:drop-shadow-xl">
-              About Us
-            </a>
-            <a href="#responsive-header" className="block m-2 ml-[55px] lg:inline-block text-white font-bold hover:drop-shadow-xl">
-              Events
-            </a>
-            <a href="#responsive-header" className="block m-2 ml-[55px] lg:inline-block text-white font-bold hover:drop-shadow-xl">
-              Contact Us
-            </a>
-            <a href="#responsive-header" className="block m-2 ml-[55px] lg:inline-block text-white font-bold hover:drop-shadow-xl">
-              Careers
-            </a>
-            <a href="#responsive-header" className="block m-2 ml-[60px] lg:inline-block text-white font-bold hover:drop-shadow-xl">
-              Signup
-            </a>
+          <a href="#responsive-header" className="block m-2 ml-[3.125rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+            Home
+          </a>
+          <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+            Team
+          </a>
+          <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+            About Us
+          </a>
+          <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+            Events
+          </a>
+          <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+            Contact Us
+          </a>
+          <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+            Careers
+          </a>
+          <a href="#responsive-header" className="block m-2 ml-[3.75rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+            Signup
+          </a>
         </div>
       </div>
     </nav>
