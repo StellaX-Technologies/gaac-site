3aef837e2ee274f00db5f33c84394afa3120711d
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-02-10 09:39:05 +0530

    Careers Page made
    1) Made the careers page.
    2) Added the respective links to the navbar to redirect to careers

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex flex-col items-center justify-center lg:bg-navbar lg:bg-opacity-11 mt-[2.0625rem] p-6 rounded-3xl max-w-[81.8125rem] mx-auto w-full">
+    <nav className="flex flex-col items-center justify-center lg:bg-navbar lg:bg-opacity-11 mt-[2.0625rem] p-5 rounded-3xl max-w-[81.8125rem] mx-auto w-full">
       <div className="flex items-center justify-between w-full">
         <div className="lg:hidden">
           <button onClick={toggleNavbar} className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-800 hover:border-gray-800">
@@ -26,7 +26,7 @@ const Navbar = () => {
           <Image src={logo} width={100} height={20} alt='logo' quality={100} />
         </div>
         <div className={`lg:flex lg:items-center lg:justify-between w-full ${isOpen ? 'block' : 'hidden'}`}>
-          <a href="#responsive-header" className="block m-2 ml-[3.125rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+          <a href="/" className="block m-2 ml-[3.125rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
             Home
           </a>
           <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
@@ -41,7 +41,7 @@ const Navbar = () => {
           <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
             Contact Us
           </a>
-          <a href="#responsive-header" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
+          <a href="careers" className="block m-2 ml-[3.4375rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
             Careers
           </a>
           <a href="#responsive-header" className="block m-2 ml-[3.75rem] lg:inline-block text-white font-bold hover:drop-shadow-xl">
